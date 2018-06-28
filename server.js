d3bcd98ce7fe97d12d0a058dea93fe6ca7f67b4e
ployandppp <ployandppp@gmail.com>
2018-06-28 14:43:38 +0800

    Setup start script and heroku port

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const hbs = require('hbs');
 const fs=require('fs')
 
+const port = process.env.PORT||3000;
 var app=express();
 
 hbs.registerPartials(__dirname+'/views/partials');
@@ -48,7 +49,7 @@ app.get('/bad',(req,res)=>{
         error_name:'Unable to get'
     })
 })
-app.listen(3000,()=>{
-    console.log('Server started')
+app.listen(port,()=>{
+    console.log('Server started on'+port)
 });
 
